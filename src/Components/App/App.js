de633794e83119928fcab471adf92235d201aaca
Glafira Zhur <glafirazhur@gmail.com>
2019-09-05 12:17:32 +0300

    Initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import './App.css';
+import Field from '../Field';
+
+function App() {
+  return (
+    <Field />
+  );
+}
+
+export default App;
