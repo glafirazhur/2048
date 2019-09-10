964f31c2731071e2b876dbe97927b83690a124d5
Glafira Zhur <glafirazhur@gmail.com>
2019-09-10 11:39:36 +0300

    Updates

--- precache-manifest.c9244806e01cb8c95c1f50dc788ab309.js
+++ precache-manifest.c9244806e01cb8c95c1f50dc788ab309.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6c54402bc4ed08f52e1cfb6f00227868",
+    "revision": "1607ad77303c9aa2949b004fa69fd58a",
     "url": "/2048/index.html"
   },
   {
-    "revision": "d640390e0ae5db63f6e1",
-    "url": "/2048/static/css/main.24d24374.chunk.css"
+    "revision": "405dae7e9377c6528a54",
+    "url": "/2048/static/css/main.6a13da7c.chunk.css"
   },
   {
     "revision": "e97319fffd16dd14c7bb",
     "url": "/2048/static/js/2.0e7deeff.chunk.js"
   },
   {
-    "revision": "d640390e0ae5db63f6e1",
-    "url": "/2048/static/js/main.979f6ede.chunk.js"
+    "revision": "405dae7e9377c6528a54",
+    "url": "/2048/static/js/main.de06eb56.chunk.js"
   },
   {
     "revision": "724cce2c8d84dd34678c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/2048/precache-manifest.1a213cd3532bfb3236dbf5cff5cbb9f2.js"
+  "/2048/precache-manifest.c9244806e01cb8c95c1f50dc788ab309.js"
 );
 
 self.addEventListener('message', (event) => {
